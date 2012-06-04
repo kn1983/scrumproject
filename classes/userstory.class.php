e99bd63e42cf51558384b4bf5aaa33c0b106ca70
Klara <kn1983@gmail.com>
2012-06-04 10:37:49 +0200

    created a class for the userstories

---
+++ classes/userstory.class.php
@@ -0,0 +1,20 @@
+<?php
+class Userstory {
+	function __construct(){
+	}
+	public function createUserStory(){
+		$cleanArgs = array();
+		if(isset($_POST['name'], $_POST['description'], $_POST['estimated_time'], $_POST['elapsed_time'], $_POST['created_date'], $_POST['checkout_by'], $_POST['checkout_date'])){
+			foreach($_POST as $key => $val){
+				$cleanArgs[$key] = $this->cleanArg($val);
+			}
+			return $cleanArgs;
+		}
+	}
+	private function cleanArg($arg){
+		$cleanArg = strip_tags($arg);
+		$cleanArg = mysql_real_escape_string($arg);
+		return $cleanArg;
+	}
+}
+?>
