fcf1c48c370a56dd801a2f8fdebee3ab67816179
Klara <kn1983@gmail.com>
2012-06-05 11:30:06 +0200

    updated the userstory class

--- classes/userstory.class.php
+++ classes/userstory.class.php
@@ -1,14 +1,17 @@
 <?php
+require_once('database.class.php');
 class Userstory {
 	function __construct(){
+		
 	}
 	public function createUserStory(){
 		$cleanArgs = array();
-		if(isset($_POST['name'], $_POST['description'], $_POST['estimated_time'], $_POST['elapsed_time'], $_POST['project_id'])){
+		if(isset($_POST['name'], $_POST['description'], $_POST['estimated_time'], $_POST['project_id'])){
 			foreach($_POST as $key => $val){
 				$cleanArgs[$key] = $this->cleanArg($val);
 			}
-			return $cleanArgs;
+			$db = new Database();
+			$db->insertUserStory($cleanArgs);
 		}
 	}
 	private function cleanArg($arg){
