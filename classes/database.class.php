e4def492f93cd9d35ef7e9915691f37f9f7a5a9f
Marc Taillefer <marctai@gmail.com>
2012-06-07 15:25:44 +0200

    database class and config

---
+++ classes/database.class.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'database_config.php';
+
+class Database {
+
+	private $connection;
+
+	function __construct() {
+		$this->openConnection();
+	}
+
+	private function openConnection() {
+		$this->connection = mysql_connect(DB_SERVER, DB_USER);
+		if (!$this->connection) {
+			die('Database connection failed ' . mysql_error());
+		}
+		else {
+			mysql_select_db(DB_NAME, $this->connection)
+				or die('Database selection failed. ' . mysql_error());
+		}
+	}
+
+	public function insertUserStory($args) {
+		if (isset($args['estimated_time']) && !$args['estimated_time']) {
+			$args['estimated_time'] = 'NULL';
+		}
+		$query = "INSERT INTO user_story
+			VALUES (NULL, '{$args['name']}', '{$args['description']}', {$args['estimated_time']}, NULL, NOW(), NULL, NULL)";
+		mysql_query($query) or die(mysql_error());
+	}
+
+	// public function insertProject($args) {
+		
+	// }
+}
---
+++ database_config.php
@@ -0,0 +1,4 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'scrumproject');
