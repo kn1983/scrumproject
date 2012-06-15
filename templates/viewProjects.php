02058c5d126387fc0a9a99d09d3ae1dad99765e9
Klara <kn1983@gmail.com>
2012-06-15 12:58:30 +0200

    created a html template for the projects

---
+++ templates/viewProjects.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Projects</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	</head>
+	<body>
+		<h1>Projects</h1>
+		<ul>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+			<li><a href="#">Lorem ipsum</a></li>
+		</ul>
+	</body>
+</html>
